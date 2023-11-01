ca0f0cfcab43246198a97e85e409822db04d77aa
sujataprasad01 <sujataprasad2004@gmail.com>
2023-11-01 20:58:12 +0530

    added images

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
 
 function App() {
   return (
     <>
+    <div className={styles.App}>
     Hello world
+    </div>
     </>
   )
 }
